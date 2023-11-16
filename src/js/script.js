0ecb5cc7502ffb9641d29c1049ab036339002ef0
Oskar Ziółkowski <oskarziolkowski25071@gmail.com>
2023-11-16 23:10:48 +0100

    Fixed links on github

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ const body = document.querySelector('body')
 const openNav = () => {
 	navbar.classList.toggle('nav__navbar--active')
     body.classList.toggle('sticky-body')
+    burgerBtn.classList.toggle('is-active')
 }
 
 burgerBtn.addEventListener('click', openNav)
