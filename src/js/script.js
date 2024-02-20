4d6a7e485fd7fca85d37878593cc81ebce5c88ee
Oskar Ziółkowski <oskarziolkowski25071@gmail.com>
2024-02-20 20:39:46 +0100

    Finished mobile view

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,26 @@
 const navbar = document.querySelector('.nav__navbar')
 const burgerBtn = document.querySelector('.hamburger')
 const body = document.querySelector('body')
+const elementsAnimated = document.querySelectorAll('.animation')
 
 const openNav = () => {
 	navbar.classList.toggle('nav__navbar--active')
-    body.classList.toggle('sticky-body')
-    burgerBtn.classList.toggle('is-active')
+	body.classList.toggle('sticky-body')
+	burgerBtn.classList.toggle('is-active')
 }
 
 burgerBtn.addEventListener('click', openNav)
+
+const observerFadeUp = new IntersectionObserver(
+	entries => {
+		entries.forEach(entry => {
+			entry.target.classList.toggle('show-fade-up', entry.isIntersecting)
+			if (entry.isIntersecting) observerFadeUp.unobserve(entry.target)
+		})
+	},
+	{ threshold: 1 }
+)
+
+elementsAnimated.forEach(el => {
+	observerFadeUp.observe(el)
+})
